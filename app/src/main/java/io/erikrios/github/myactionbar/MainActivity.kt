87a4786bc73d062dc210a319046e035c1d559b99
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-30 09:41:01 +0700

    Integrated the view binding

--- app/src/main/java/io/erikrios/github/myactionbar/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/myactionbar/MainActivity.kt
@@ -2,10 +2,16 @@ package io.erikrios.github.myactionbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.erikrios.github.myactionbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
